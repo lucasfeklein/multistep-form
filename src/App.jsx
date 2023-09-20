9742019f9de5224f4bf51cb4aa1b20ee67d7468f
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-20 15:05:49 +0800

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,27 @@ function App() {
             <p className="step-title">YOUR INFO</p>
           </div>
         </div>
+        <div className="choice-container">
+          <button className="step-btn">2</button>
+          <div>
+            <p className="step-number">STEP 2</p>
+            <p className="step-title">SELECT PLAN</p>
+          </div>
+        </div>
+        <div className="choice-container">
+          <button className="step-btn">3</button>
+          <div>
+            <p className="step-number">STEP 3</p>
+            <p className="step-title">ADD-ONS</p>
+          </div>
+        </div>
+        <div className="choice-container">
+          <button className="step-btn">4</button>
+          <div>
+            <p className="step-number">STEP 4</p>
+            <p className="step-title">SUMMARY</p>
+          </div>
+        </div>
       </div>
       <div></div>
     </div>
