09cf26c0f7cc767b570a9b352365ca4499d201c3
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-20 14:29:16 +0800

    choice basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <div className="container">
-      <div className="step-container"></div>
+      <div className="step-container">
+        <div className="choice-container">
+          <button>1</button>
+          <div>
+            <p>STEP 1</p>
+            <p>YOUR INFO</p>
+          </div>
+        </div>
+      </div>
       <div></div>
     </div>
   );
