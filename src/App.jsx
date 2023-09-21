6216aa69fdbb9bc74366ab56615becb4bab132de
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-21 10:58:36 +0800

    steps components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { Step1, Step2, Step3, Step4 } from "./components";
 import SidebarButton from "./components/SidebarButton";
 import { steps } from "./utils/variables";
 
 function App() {
-  const [activeStep, setActiveStep] = useState();
+  const [activeStep, setActiveStep] = useState(1);
 
   const handleClick = (step) => {
     setActiveStep(step);
@@ -22,7 +23,12 @@ function App() {
           />
         ))}
       </div>
-      <div className="plan-container"></div>
+      <div className="plan-container">
+        {activeStep === 1 && <Step1 />}
+        {activeStep === 2 && <Step2 />}
+        {activeStep === 3 && <Step3 />}
+        {activeStep === 4 && <Step4 />}
+      </div>
     </div>
   );
 }
