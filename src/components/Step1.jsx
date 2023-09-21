72a9edc210281d783b3ec6b7e6092ff78f91c6ec
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-21 16:10:06 +0800

    button added

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -1,4 +1,4 @@
-import { Box, Text, TextInput, Title } from "@mantine/core";
+import { Box, Button, Text, TextInput, Title } from "@mantine/core";
 
 const labelStyle = {
   color: "hsl(213, 96%, 18%)",
@@ -15,35 +15,56 @@ const inputStyle = {
 
 const Step1 = () => {
   return (
-    <Box pt={30}>
-      <Title style={{ color: "hsl(213, 96%, 18%)" }}>Personal info</Title>
-      <Text color="hsl(231, 11%, 63%)">
-        Please provide your name, email address and phone number.
-      </Text>
-      <TextInput
-        mt={25}
-        styles={{
-          label: labelStyle,
-          input: inputStyle,
+    <Box
+      pt={30}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "100%",
+      }}
+    >
+      <Box>
+        <Title style={{ color: "hsl(213, 96%, 18%)" }}>Personal info</Title>
+        <Text color="hsl(231, 11%, 63%)">
+          Please provide your name, email address and phone number.
+        </Text>
+        <TextInput
+          mt={25}
+          styles={{
+            label: labelStyle,
+            input: inputStyle,
+          }}
+          label="Name"
+          placeholder="John Doe"
+          type="text"
+        />
+        <TextInput
+          mt={15}
+          styles={{ label: labelStyle, input: inputStyle }}
+          label="Email Address"
+          placeholder="example@example.com"
+          type="email"
+        />
+
+        <TextInput
+          mt={15}
+          styles={{ label: labelStyle, input: inputStyle }}
+          label="Phone Number"
+          placeholder="e.g. +1 234 567 890"
+          type="text"
+        />
+      </Box>
+      <Button
+        mb={20}
+        style={{
+          alignSelf: "end",
+          backgroundColor: "hsl(213, 96%, 18%)",
+          fontWeight: "500",
         }}
-        label="Name"
-        placeholder="John Doe"
-        type="text"
-      />
-      <TextInput
-        mt={15}
-        styles={{ label: labelStyle, input: inputStyle }}
-        label="Email Address"
-        placeholder="example@example.com"
-        type="email"
-      />
-      <TextInput
-        mt={15}
-        styles={{ label: labelStyle, input: inputStyle }}
-        label="Phone Number"
-        placeholder="e.g. +1 234 567 890"
-        type="text"
-      />
+      >
+        Next Step
+      </Button>
     </Box>
   );
 };
