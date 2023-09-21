5e22836158d31f9811940db1411b42927c0daa71
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-21 10:37:57 +0800

    active button styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import SidebarButton from "./components/SidebarButton";
 import { steps } from "./utils/variables";
 
 function App() {
+  const [activeStep, setActiveStep] = useState();
+
+  const handleClick = (step) => {
+    setActiveStep(step);
+  };
+
   return (
     <div className="container">
       <div className="sidebar-container">
         {steps.map(({ step, title }) => (
-          <SidebarButton key={step} step={step} title={title} />
+          <SidebarButton
+            key={step}
+            step={step}
+            title={title}
+            isActive={activeStep === step}
+            handleClick={() => handleClick(step)}
+          />
         ))}
       </div>
       <div className="plan-container"></div>
--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,7 +1,14 @@
-const SidebarButton = ({ step, title }) => {
+const SidebarButton = ({ step, title, isActive, handleClick }) => {
+  const buttonStyle = {
+    color: isActive ? "black" : "white",
+    background: isActive ? "hsl(206, 94%, 87%)" : "none",
+  };
+
   return (
     <div className="choice-container">
-      <button className="step-btn">{step}</button>
+      <button className="step-btn" onClick={handleClick} style={buttonStyle}>
+        {step}
+      </button>
       <div>
         <p className="step-number">STEP {step}</p>
         <p className="step-title">{title}</p>
