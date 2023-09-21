3a5354db164c344e96400bc8ff89482e295d3935
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-21 15:46:35 +0800

    step1 content

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -1,5 +1,38 @@
+import { Box, Text, TextInput, Title } from "@mantine/core";
+
+const labelStyle = { color: "hsl(213, 96%, 18%)", fontWeight: "400" };
+const inputStyle = { color: "hsl(213, 96%, 18%)", fontWeight: "500" };
+
 const Step1 = () => {
-  return <div>Step1</div>;
+  return (
+    <Box pt={30}>
+      <Title style={{ color: "hsl(213, 96%, 18%)" }}>Personal info</Title>
+      <Text color="hsl(231, 11%, 63%)">
+        Please provide your name, email address and phone number.
+      </Text>
+      <TextInput
+        styles={{
+          label: labelStyle,
+          input: { color: "hsl(213, 96%, 18%)", fontWeight: "500" },
+        }}
+        label="Name"
+        placeholder="John Doe"
+        type="text"
+      />
+      <TextInput
+        styles={{ label: labelStyle }}
+        label="Email Address"
+        placeholder="example@example.com"
+        type="email"
+      />
+      <TextInput
+        styles={{ label: labelStyle }}
+        label="Phone Number"
+        placeholder="e.g. +1 234 567 890"
+        type="text"
+      />
+    </Box>
+  );
 };
 
 export default Step1;
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MantineProvider>
+    <MantineProvider theme={{ fontFamily: "Ubuntu, sans-serif" }}>
       <App />
     </MantineProvider>
   </React.StrictMode>
