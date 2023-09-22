df570c43ab02163dcac9e119a264fa0c348eb369
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-22 15:13:49 +0800

    step 3 cards

--- src/components/Step3/Step3.jsx
+++ src/components/Step3/Step3.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, Text, Title } from "@mantine/core";
+import { Box, Button, Card, Flex, Text, Title } from "@mantine/core";
 import React from "react";
 import classes from "./Step3.module.css";
 
@@ -17,14 +17,43 @@ const Step3 = ({ handleClick, handleFormData }) => {
         <Text style={{ color: "hsl(231, 11%, 63%)" }}>
           Add-ons help enhance your gaming experience.
         </Text>
-        <Box style={{ marginTop: "25px", display: "flex", gap: "20px" }}>
-          <Card>
-            <input type="checkbox" />
-            <Box>
-              <h2>Online service</h2>
-              <p>Online service</p>
-            </Box>
+        <Box
+          style={{
+            marginTop: "25px",
+            display: "flex",
+            gap: "20px",
+            flexDirection: "column",
+          }}
+        >
+          <Card className={classes.card}>
+            <Flex gap={20}>
+              <input type="checkbox" />
+              <Box>
+                <h2>Online service</h2>
+                <p>Online service</p>
+              </Box>
+            </Flex>
             <p>+1/mo</p>
+          </Card>
+          <Card className={classes.card}>
+            <Flex gap={20}>
+              <input type="checkbox" />
+              <Box>
+                <h2>Larger storage</h2>
+                <p>Extra 1TB of cloud save</p>
+              </Box>
+            </Flex>
+            <p>+2/mo</p>
+          </Card>
+          <Card className={classes.card}>
+            <Flex gap={20}>
+              <input type="checkbox" />
+              <Box>
+                <h2>Customizable profile</h2>
+                <p>Custom theme on your profile</p>
+              </Box>
+            </Flex>
+            <p>+2/mo</p>
           </Card>
         </Box>
       </Box>
