1d1b774f777c372df92c3eba53dc8f71ffd02a51
Lucas Klein <lucas.fklein@hotmail.com>
2023-09-22 12:59:01 +0800

    active card step 2

--- src/components/Step2/Step2.jsx
+++ src/components/Step2/Step2.jsx
@@ -6,7 +6,6 @@ const Step2 = ({ handleClick, plan, handleFormData }) => {
   const handleNextStepClick = () => {
     handleClick(3);
   };
-
   const handleBackStepClick = () => {
     handleClick(1);
   };
@@ -18,17 +17,29 @@ const Step2 = ({ handleClick, plan, handleFormData }) => {
           You have the choice of monthly or yearly billing.
         </Text>
         <Box style={{ marginTop: "25px", display: "flex", gap: "20px" }}>
-          <Card className={classes.card} id="arcade">
+          <Card
+            className={`${classes.card} ${
+              plan === "arcade" ? classes.active_card : ""
+            }`}
+          >
             <Image src="./icon-arcade.svg" w={35} mb={50} />
             <h2>Arcade</h2>
             <p>$9/mo</p>
           </Card>
-          <Card className={classes.card} id="advanced">
+          <Card
+            className={`${classes.card} ${
+              plan === "advanced" ? classes.active_card : ""
+            }`}
+          >
             <Image src="./icon-advanced.svg" w={35} mb={50} />
             <h2>Advanced</h2>
             <p>$12/mo</p>
           </Card>
-          <Card className={classes.card} id="pro">
+          <Card
+            className={`${classes.card} ${
+              plan === "pro" ? classes.active_card : ""
+            }`}
+          >
             <Image src="./icon-pro.svg" w={35} mb={50} />
             <h2>Pro</h2>
             <p>$15/mo</p>
